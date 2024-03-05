8348c24f2c878d3792dda09d89ae93b37a4c254e
Corentinpgt <corentin.pouget.19@gmail.com>
2024-03-05 14:23:54 +0100

    chrono js fait

---
+++ script.js
@@ -0,0 +1,49 @@
+let Chrono = {
+    time : 5,
+    convert : {
+        minute : 0,
+        seconde : 0,
+    }
+}
+
+
+function chrono() {
+    
+    function chronoDecr() {
+        if (Chrono.time > 0) {
+            Chrono.time--;
+            chronoConvert();
+            chronoRender();
+            console.log(Chrono.time);
+        }
+        else {
+            let message = document.querySelector("#chrono__end");
+            message.style.display = "block";
+        }
+    }
+        
+    function chronoConvert() {
+        Chrono.convert.minute = Math.floor(Chrono.time / 60);
+        Chrono.convert.seconde = Chrono.time % 60;
+    }
+    
+    function chronoRender() {
+        let template = document.querySelector("#template__duree").innerHTML;
+        template = template.replace("{{minute}}",Chrono.convert.minute);
+        template = template.replace("{{seconde}}",Chrono.convert.seconde);
+        document.querySelector("#chrono__container").innerHTML = template;
+    }
+        
+    setInterval(chronoDecr, 1000);
+        
+
+}
+
+let chrono_btn = document.querySelector("#chrono__btn");
+
+function handlerChrono() {
+    chrono_btn.style.display = "none";
+    chrono();
+}
+
+chrono_btn.addEventListener("click", handlerChrono);
